dd1185171b8f7936a77a899c1265d7c8cc334265
Luan Coêlho <luancoelho.dev@gmail.com>
2024-06-03 02:54:35 -0300

    feat: update.

--- atividade_aula_12/atividade_docker/frontend/src/App.js
+++ atividade_aula_12/atividade_docker/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import MessageForm from "./components/MessageForm";
 import MessageList from "./components/MessageList";
+import "./index.css";
 
 function App() {
   const [messages, setMessages] = useState([]);
@@ -14,12 +15,18 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Internal Communication App</h1>
-      </header>
-      <MessageForm setMessages={setMessages} />
-      <MessageList messages={messages} />
+    <div className="App w-full h-screen bg-gray-100">
+      <div className="container mx-auto p-3">
+        <header className="text-center mb-6">
+          <h1 className="text-3xl font-semibold text-gray-800">Internal Communication App</h1>
+        </header>
+        <div className="bg-white shadow-md rounded-lg p-6">
+          <MessageForm setMessages={setMessages} />
+        </div>
+        <div className="mt-6">
+          <MessageList messages={messages} />
+        </div>
+      </div>
     </div>
   );
 }
--- atividade_aula_12/atividade_docker/frontend/src/components/MessageForm.js
+++ atividade_aula_12/atividade_docker/frontend/src/components/MessageForm.js
@@ -4,6 +4,8 @@ function MessageForm({ setMessages }) {
   const [content, setContent] = useState("");
 
   const handleSubmit = (event) => {
+    event.preventDefault();
+
     const baseUrl = process.env.REACT_APP_API_BASE_URL;
     fetch(`${baseUrl}/api/messages`, {
       method: "POST",
@@ -22,13 +24,23 @@ function MessageForm({ setMessages }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        placeholder="Type a message"
-      />
-      <button type="submit">Send</button>
+      <div>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Type a message"
+        />
+        <div className="flex justify-end mt-2">
+          <button
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            type="submit"
+          >
+            Send
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
